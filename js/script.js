7eb7e0b9e6aeeb9b1fb84101b734d67bee1c747a
ProkofyevM <88980620+ProkofyevM@users.noreply.github.com>
2022-06-15 16:39:56 +0300

    added counter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const day = new Date('Jun 16 2022 00:00:00');
+   //console.log(day.setDate(day.getDate() ));
+
+   const hoursVal = document.querySelector('.time-count__hours .time-count__val');
+   const minutesVal = document.querySelector('.time-count__minutes .time-count__val');
+   const secondsVal = document.querySelector('.time-count__seconds .time-count__val');
+
+   const hoursText = document.querySelector('.time-count__hours .time-count__text');
+   const minutesText = document.querySelector('.time-count__minutes .time-count__text');
+   const secondsText = document.querySelector('.time-count__seconds .time-count__text');
+
+   function declOfNum(number, titles) {  
+      let cases = [2, 0, 1, 1, 1, 2];  
+      return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
+   }
+
+   const timeCount = () => {
+      let now = new Date();
+      let leftUntil = day - now;
+
+      let hours = Math.floor(leftUntil / 1000 / 60 / 60) % 24;
+      let minutes = Math.floor(leftUntil / 1000 / 60) % 60;
+      let seconds = Math.floor(leftUntil / 1000) % 60;
+
+      (hours < 10) ? hours = `0 ${hours}` : 'help';
+      (minutes < 10) ? minutes = `0 ${minutes}` : 'help';
+      (seconds < 10) ? seconds = `0 ${seconds}` : 'help';
+
+      hoursVal.textContent = hours;
+      minutesVal.textContent = minutes;
+      secondsVal.textContent = seconds;
+
+
+      hoursText.textContent = declOfNum (hours, ['час', 'часа', 'часов']);
+      minutesText.textContent = declOfNum (minutes, ['минута', 'минуты', 'минут']);
+      secondsText.textContent = declOfNum (seconds, ['секунда', 'секунды', 'секунд']);
+   }
+   timeCount();
+   setInterval(timeCount, 1000);
+});
+
 const bt = document.querySelector('.bt');
 const input = document.querySelector('.inp');
 const ul = document.querySelector('.list');
